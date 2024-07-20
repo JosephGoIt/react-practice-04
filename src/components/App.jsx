9c4f667aa786a4d5d6c3908fad402791cd5c6bcf
Joseph Camacho <spongkj@gmail.com>
2024-07-20 17:06:44 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,21 @@ import favoriteBooksData from './BookLib/favoriteBooks.json';
 
 export const App = () => {
   const [favoriteBooks, setFavoriteBooks] = useState([]);
+  const [value, setValue] = useState(0);
 
   useEffect(() => {
     // Directly set the imported JSON data
     setFavoriteBooks(favoriteBooksData);
-  }, []);
+    // console.log(value);
+    console.log("Mounting phase: same when componentDidUpdate runs");
+  }, [value]);
 
 
   return (
     <div>
       <h1>Favorite Books</h1>
       <BookList books={favoriteBooks} />
+      <button onClick={() => setValue(value+1)}>{value}</button>
     </div>
   );
 };
